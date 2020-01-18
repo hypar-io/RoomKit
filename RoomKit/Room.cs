d11cd6cbf12437a51d852276654fbd383c4e05c1
hummingburd <anthony@hypar.io>
2020-01-17 22:17:22 -0500

    Update Room.cs

--- RoomKit/Room.cs
+++ RoomKit/Room.cs
@@ -20,11 +20,12 @@ namespace RoomKit
         {
 
             Color = Palette.White;
+            Elevation = 0.0;
+            Height = 1.0;
             Name = "";
             Number = "";
             Perimeter = Polygon.Rectangle(Vector3.Origin, new Vector3(1.0, 1.0));
-            Position = Vector3.Origin;
-            Size = new Vector3(1.0, 1.0, 1.0);
+            SuiteID = "";
             UniqueID = Guid.NewGuid().ToString();
         }
 
@@ -33,13 +34,13 @@ namespace RoomKit
         /// </summary>
         public Room(Vector3 size)
         {
-
             Color = Palette.White;
+            Elevation = 0.0;
+            Height = size.Z;
             Name = "";
             Number = "";
             Perimeter = Polygon.Rectangle(Vector3.Origin, new Vector3(size.X, size.Y));
-            Position = Vector3.Origin;
-            Size = size;
+            SuiteID = "";
             UniqueID = Guid.NewGuid().ToString();
         }
 
@@ -48,13 +49,13 @@ namespace RoomKit
         /// </summary>
         public Room(double area, double ratio, double height)
         {
-
             Color = Palette.White;
+            Elevation = 0.0;
+            Height = height;
             Name = "";
             Number = "";
             Perimeter = Shaper.RectangleByArea(area, ratio);
-            Position = Vector3.Origin;
-            Size = new Vector3(Perimeter.Compass().SizeX, Perimeter.Compass().SizeY, height);
+            SuiteID = "";
             UniqueID = Guid.NewGuid().ToString();
         }
 
@@ -63,13 +64,13 @@ namespace RoomKit
         /// </summary>
         public Room(Vector3 start, Vector3 end, double width, double height)
         {
-
             Color = Palette.White;
+            Elevation = 0.0;
+            Height = height;
             Name = "";
             Number = "";
             Perimeter = new Line(start, end).Thicken(width);
-            Position = Vector3.Origin;
-            Size = new Vector3(Perimeter.Compass().SizeX, Perimeter.Compass().SizeY, height);
+            SuiteID = "";
             UniqueID = Guid.NewGuid().ToString();
         }
 
@@ -78,13 +79,13 @@ namespace RoomKit
         /// </summary>
         public Room(Line line, double width, double height)
         {
-
             Color = Palette.White;
+            Elevation = 0.0;
+            Height = height;
             Name = "";
             Number = "";
             Perimeter = line.Thicken(width);
-            Position = Vector3.Origin;
-            Size = new Vector3(Perimeter.Compass().SizeX, Perimeter.Compass().SizeY, height);
+            SuiteID = "";
             UniqueID = Guid.NewGuid().ToString();
         }
 
@@ -95,11 +96,12 @@ namespace RoomKit
         {
 
             Color = Palette.White;
+            Elevation = 0.0;
+            Height = height;
             Name = "";
             Number = "";
             Perimeter = polygon;
-            Position = Vector3.Origin;
-            Size = new Vector3(Perimeter.Compass().SizeX, Perimeter.Compass().SizeY, height);
+            SuiteID = "";
             UniqueID = Guid.NewGuid().ToString();
         }
 
@@ -110,11 +112,12 @@ namespace RoomKit
         {
 
             Color = room.Color;
+            Elevation = room.Elevation;
+            Height = room.Height;
             Name = room.Name;
             Number = room.Number;
             Perimeter = room.Perimeter;
-            Position = room.Position;
-            Size = room.Size;
+            SuiteID = room.SuiteID;
             UniqueID = Guid.NewGuid().ToString();
         }
 
@@ -123,64 +126,13 @@ namespace RoomKit
         #region Properties
 
         /// <summary>
-        /// The area of the room's perimeter Polygon. 
-        /// Returns -1.0 if the Room's Perimeter is null.
+        /// The area of the Room's Perimeter;
         /// </summary>
         public double Area
         {
             get
             {
                 return Math.Abs(Perimeter.Area());
-            }
-        }
-
-        /// <summary>
-        /// A Mass created from Room characteristics.
-        /// </summary>
-        public Mass AsMass
-        {
-            get
-            {
-                return new Mass(new Profile(Perimeter),
-                                Height,
-                                new Material(Guid.NewGuid().ToString(), Color),
-                                new Transform(Position),
-                                null,
-                                Guid.NewGuid(),
-                                Name);
-            }
-        }
-
-        /// <summary>
-        /// A Panel created from Room characteristics.
-        /// </summary>
-        public Panel AsPanel
-        {
-            get
-            {
-                return new Panel(Perimeter,
-                                 new Material(Guid.NewGuid().ToString(), Color),
-                                 new Transform(Position),
-                                 null,
-                                 Guid.NewGuid(),
-                                 Name);
-            }
-        }
-
-        /// <summary>
-        /// A Space created from Room characteristics.
-        /// </summary>
-        public Space AsSpace
-        {
-            get
-            {
-                return new Space(new Profile(Perimeter),
-                                 Height,
-                                 new Material(Guid.NewGuid().ToString(), Color),
-                                 new Transform(Position),
-                                 null,
-                                 Guid.NewGuid(),
-                                 Name);
             }
         }
 
@@ -192,12 +144,30 @@ namespace RoomKit
         /// <summary>
         /// Vertical position of the Room's lowest plane relative to the ground plane.
         /// </summary> 
-        public double Elevation { get; }
+        public double Elevation { get; set; }
 
         /// <summary>
         /// Vertical height of the room above its elevation.
-        /// </summary> 
-        public double Height { get; }
+        /// </summary>
+        private double height;
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                if (value > 0.0)
+                {
+                    height = value;
+                }
+                else
+                {
+                    height = 1.0;
+                }
+            }
+        }
 
         /// <summary>
         /// Arbitrary string identifier for this Room instance.
@@ -212,17 +182,34 @@ namespace RoomKit
         /// <summary>
         /// Polygon perimeter of the Room.
         /// </summary>
-        public Polygon Perimeter { get; private set; }
+        private Polygon perimeter;
+        public Polygon Perimeter
+        {
+            get
+            {
+                return perimeter;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    return;
+                }
+                if (value.IsClockWise())
+                {
+                    perimeter = value.Reversed();
+                }
+                else
+                {
+                    perimeter = value;
+                }
+            }
+        }
 
         /// <summary>
-        /// Polygon perimeter of the Room.
+        /// Arbitrary string identifier for this Room instance.
         /// </summary>
-        public Vector3 Position { get; set; }
-
-        /// <summary>
-        /// Dimensions of the Room Perimeter orthogonal bounding box.
-        /// </summary>
-        public Vector3 Size { get; private set; }
+        public string SuiteID { get; set; }
 
         /// <summary>
         /// UUID for this instance, set on initialization.
@@ -237,14 +224,26 @@ namespace RoomKit
         /// Rotates the Room Perimeter in the horizontal plane around the supplied pivot point.
         /// </summary>
         /// <param name="pivot">Vector3 point around which the Room Perimeter will be rotated.</param> 
-        /// <param name="angle">Angle in degrees to rotate the Perimeter.</param> 
+        /// <param name="rotation">Angle in degrees to rotate the Perimeter.</param> 
         /// <returns>
-        /// True if the Perimeter is successfully rotated.
+        /// None.
         /// </returns>
-        public void Rotate(Vector3 pivot, double angle)
+        public void MoveFromTo(Vector3 from, Vector3 to)
         {
-            Perimeter = Perimeter.Rotate(pivot, angle);
-            Size = new Vector3(Perimeter.Compass().SizeX, Perimeter.Compass().SizeY, Size.Z);
+            Perimeter = perimeter.MoveFromTo(from, to);
+        }
+
+        /// <summary>
+        /// Rotates the Room Perimeter in the horizontal plane around the supplied pivot point.
+        /// </summary>
+        /// <param name="pivot">Vector3 point around which the Room Perimeter will be rotated.</param> 
+        /// <param name="rotation">Angle in degrees to rotate the Perimeter.</param> 
+        /// <returns>
+        /// None.
+        /// </returns>
+        public void Rotate(Vector3 pivot, double rotation)
+        {
+            Perimeter = perimeter.Rotate(pivot, rotation);
         }
 
         #endregion
